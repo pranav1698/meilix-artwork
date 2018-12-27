14f54472959befdcbffff7593cd2287ed9a327f1
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2018-12-27 23:00:25 +0530

    test-deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   git config --global user.name "Travis CI" 
 
   # Clone the repository in the buildApk folder
-  git clone --quiet --branch=deb https://abishekvashok:$GITHUB_API_KEY@github.com/fossasia/meilix-artwork deb > /dev/null
+  git clone --quiet --branch=deb https://pranav1698:$GITHUB_API_KEY@github.com/fossasia/meilix-artwork deb > /dev/null
 
   cp -Rf $TRAVIS_BUILD_DIR/plymouth-theme-meilix-logo_*_all.deb deb/
   cp -Rf $TRAVIS_BUILD_DIR/plymouth-theme-meilix-text_*_all.deb deb/
